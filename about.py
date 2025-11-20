78da0318e9c0dbd77d7bc5fb2de75bafc7c03df8
Kelsi Flatland <kelsi.flatland@gmail.com>
2025-11-20 11:34:43 -0800

    add light pollution snapshots to the about page

--- about.py
+++ about.py
@@ -23,3 +23,13 @@ st.markdown("""
 - Nico Leffel - [LinkedIn](https://www.linkedin.com/in/nicoleffel/) - [Github](https://github.com/leffel)
 - Rhona Nyakulama - [LinkedIn](https://www.linkedin.com/in/rhona-nyakulama-2b486a25a/)
 """)
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.image('assets/nighttime_kigali.png', caption='Nighttime light pollution over Kigali, Rwanda (summer 2024)', use_column_width=True)
+  st.image('assets/nighttime_nyc.png', caption='Nighttime light pollution over New York City, US (summer 2024)', use_column_width=True)
+with col2:
+  st.image('assets/nighttime_sf.png', caption='Nighttime light pollution over San Francisco, US (summer 2024)', use_column_width=True)
+  st.image('assets/nighttime_seattle.png', caption='Nighttime light pollution over Seattle, US (summer 2024)', use_column_width=True)
