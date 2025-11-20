620b5ae0bbe4ed37654dbcda8bd860cedf3bd918
Kelsi Flatland <kelsi.flatland@gmail.com>
2025-11-20 12:26:55 -0800

    minor fix to about page

--- about.py
+++ about.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 st.header('Light Pollution x Air Pollution')
-st.subheader('About this project')
-# TODO: add project description
+
 st.write('Terra.do Software x Climate Course Final Project - Fall 2025')
+
+st.subheader('About this project')
 
 st.markdown("""
 This project attempts to quantify and visualize the potential relationship between   
@@ -30,7 +31,7 @@ col1, col2 = st.columns(2)
 
 with col1:
   st.image('assets/nighttime_kigali.png', caption='Nighttime light pollution over Kigali, Rwanda (summer 2024)', use_column_width=True)
-  st.image('assets/nighttime_nyc.png', caption='Nighttime light pollution over New York City, US (summer 2024)', use_column_width=True)
-with col2:
   st.image('assets/nighttime_sf.png', caption='Nighttime light pollution over San Francisco, US (summer 2024)', use_column_width=True)
+with col2:
+  st.image('assets/nighttime_nyc.png', caption='Nighttime light pollution over New York City, US (summer 2024)', use_column_width=True)
   st.image('assets/nighttime_seattle.png', caption='Nighttime light pollution over Seattle, US (summer 2024)', use_column_width=True)
